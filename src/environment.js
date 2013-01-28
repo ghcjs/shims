fe57afa95b1a7804a617421e0b4221a38112831a
Luite Stegeman <stegeman@gmail.com>
2013-01-28 14:59:23 +0100

    errno support

--- src/environment.js
+++ src/environment.js
@@ -1,3 +1,14 @@
 function getProgArgv(pid,pio,psd,pso) {
   pid.setInt32(pio, 0);
 }
+
+var $hs_errno = 0;
+function __hscore_get_errno() {
+  log("### __hscore_get_errno: " + $hs_errno);
+  return $hs_errno; 
+}
+
+function strerror(err) {
+  ret1 = 0;
+  return encodeUtf8("error");
+}
