50d187b19d5acff91cc0c4c1291e75eff1ec4e7b
Luite Stegeman <stegeman@gmail.com>
2013-01-30 15:06:09 +0100

    add errorBelch error reporting for uncaught exceptions

--- src/environment.js
+++ src/environment.js
@@ -12,3 +12,27 @@ function strerror(err) {
   ret1 = 0;
   return encodeUtf8("error");
 }
+
+function errorBelch() {
+  log("### errorBelch: do we need to handle a vararg function here?");
+}
+
+function errorBelch2(buf1, buf_offset1, buf2, buf_offset2) {
+//  log("### errorBelch2");
+  errorMsg(decodeUtf8z(buf1, buf_offset1), decodeUtf8z(buf2, buf_offset2));
+}
+
+function errorMsg(pat) {
+  // poor man's vprintf
+  var str = pat;
+  for(var i=1;i<arguments.length;i++) {
+    str = str.replace(/%s/, arguments[i]);
+  }
+  if(typeof process !== 'undefined' && process && process.stderr) {
+    process.stderr.write(str+"\n");
+  } else if (typeof putstr !== 'undefined') {
+    putstr(str+"\n");
+  } else if(typeof(console) !== 'undefined') {
+    console.log(str);
+  }
+}
