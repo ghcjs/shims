eb8fb2e68445b1080ce8432677c5e4a568579d9c
Luite Stegeman <stegeman@gmail.com>
2013-02-08 14:34:36 +0100

    also copy second half of md5 digest

--- src/md5.js
+++ src/md5.js
@@ -10,7 +10,9 @@ function MD5Update(ctx, data, len) {
 function MD5Final(dst, ctx) {
   var digest = ctx.md5ctx.digest();
   var dv = new DataView(digest);
-  dst.putUint32(0, dv.getUint32(0));
-  dst.putUint32(4, dv.getUint32(4));
+  dst.putUint32(0,  dv.getUint32(0));
+  dst.putUint32(4,  dv.getUint32(4));
+  dst.putUint32(8,  dv.getUint32(8));
+  dst.putUint32(12, dv.getUint32(12));
 }
 
