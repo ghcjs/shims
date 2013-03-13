add3296a8651258c977b539d0a2ec6eea4e514af
Luite Stegeman <stegeman@gmail.com>
2013-03-13 01:50:09 +0100

    command line argument support for node.js and SpiderMonkey

--- src/environment.js
+++ src/environment.js
@@ -1,13 +1,31 @@
-// fixme node argv
-function h$getProgArgv(pid,pio,psd,pso) {
-/*  if( ) {
-    pid.setInt32(pio, 0);
-  } else if {
-    pid.setInt32(pio, 0);
-  } */
-  pid.setInt32(pio, 0);
+var h$programArgs;
+if(typeof scriptArgs !== 'undefined') {
+  h$programArgs = scriptArgs.slice(0);
+  h$programArgs.unshift("a.js");
+} else if(typeof process !== 'undefined' && process.argv) {
+  h$programArgs = process.argv.slice(1);
+} else if(typeof arguments !== 'undefined') {
+  h$programArgs = arguments.slice(0);
+  h$programArgs.unshift("a.js");
+} else {
+  h$programArgs = [ "a.js" ];
 }
 
+function h$getProgArgv(argc_v,argc_off,argv_v,argv_off) {
+  var c = h$programArgs.length;
+  if(c === 0) {
+    argc_v.setInt32(argc_off, 0);
+  } else {
+    argc_v.setInt32(argc_off, c);
+    var argv = new DataView(new ArrayBuffer(4*c));
+    argv.arr = [];
+    for(var i=0;i<h$programArgs.length;i++) {
+      argv.arr[4*i] = [ h$encodeUtf8(h$programArgs[i]), 0 ];
+    }
+    if(!argv_v.arr) { argv_v.arr = []; }
+    argv_v.arr[argv_off] = [argv, 0];
+  }
+}
 
 function h$errorBelch() {
   log("### errorBelch: do we need to handle a vararg function here?");
