7bf70edb9715e26e61cc10a60d4fa1fc264798bd
Luite Stegeman <stegeman@gmail.com>
2013-03-25 18:55:59 +0100

    hashable_wang_32

---
+++ src/hashable.js
@@ -0,0 +1,28 @@
+/*
+ * These hash functions were developed by Thomas Wang.
+ *
+ * http://www.concentric.net/~ttwang/tech/inthash.htm
+ */
+
+function h$hashable_wang_32(a) {
+  a = (a ^ 61) ^ (a >> 16);
+  a = a + (a << 3);
+  a = a ^ (a >> 4);
+  a = a * 0x27d4eb2d;
+  a = a ^ (a >> 15);
+  return a;
+}
+
+/*
+uint64_t hashable_wang_64(uint64_t key)
+{
+    key = (~key) + (key << 21); // key = (key << 21) - key - 1;
+    key = key ^ ((key >> 24) | (key << 40));
+    key = (key + (key << 3)) + (key << 8); // key * 265
+    key = key ^ ((key >> 14) | (key << 50));
+    key = (key + (key << 2)) + (key << 4); // key * 21
+    key = key ^ ((key >> 28) | (key << 36));
+    key = key + (key << 31);
+    return key;
+}
+*/
