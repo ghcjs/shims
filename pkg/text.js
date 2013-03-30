e7a82da3dc500b202a8594a4c2b0000be77f180a
Luite Stegeman <stegeman@gmail.com>
2013-03-30 06:25:45 +0100

    support the text package

---
+++ pkg/text.js
@@ -0,0 +1,97 @@
+function h$_hs_text_memcpy(dst_v,dst_o2,src_v,src_o2,n) {
+  return h$memcpy(dst_v,2*dst_o2,src_v,2*src_o2,2*n);
+}
+
+function h$_hs_text_memcmp(a_v,a_o2,b_v,b_o2,n) {
+  return h$memcmp(a_v,2*a_o2,b_v,2*b_o2,2*n);
+}
+
+// decoder below adapted from cbits/cbits.c in the text package
+
+/**
+ * @define {number} size of Word and Int. If 64 we use goog.math.Long.
+ */
+var h$_text_UTF8_ACCEPT = 0;
+/**
+ * @define {number} size of Word and Int. If 64 we use goog.math.Long.
+ */
+var h$_text_UTF8_REJECT = 12
+
+var h$_text_utf8d =
+   [
+  /*
+   * The first part of the table maps bytes to character classes that
+   * to reduce the size of the transition table and create bitmasks.
+   */
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+   7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+   8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+  10,3,3,3,3,3,3,3,3,3,3,3,3,4,3,3, 11,6,6,6,5,8,8,8,8,8,8,8,8,8,8,8,
+
+  /*
+   * The second part is a transition table that maps a combination of
+   * a state of the automaton and a character class to a state.
+   */
+   0,12,24,36,60,96,84,12,12,12,48,72, 12,12,12,12,12,12,12,12,12,12,12,12,
+  12, 0,12,12,12,12,12, 0,12, 0,12,12, 12,24,12,12,12,12,12,24,12,24,12,12,
+  12,12,12,12,12,12,12,24,12,12,12,12, 12,24,12,12,12,12,12,12,12,24,12,12,
+  12,12,12,12,12,12,12,36,12,36,12,12, 12,36,12,12,12,12,12,36,12,36,12,12,
+  12,36,12,12,12,12,12,12,12,12,12,12];
+
+/*
+ * A best-effort decoder. Runs until it hits either end of input or
+ * the start of an invalid byte sequence.
+ *
+ * At exit, updates *destoff with the next offset to write to, and
+ * returns the next source offset to read from.
+ */
+
+function h$_hs_text_decode_utf8( dest_v
+                               , destoff_v, destoff_o
+                               , src_v, src_o
+                               , src_end_v, src_end_o
+                               ) {
+  var dsto = destoff_v.getUint32(destoff_o);
+  var srco = src_o;
+  var state = h$_text_UTF8_ACCEPT;
+  var codepoint;
+
+  function decode(byte) {
+    var type = h$_text_utf8d[byte];
+    codepoint = (state !== h$_text_UTF8_ACCEPT) ?
+      (byte & 0x3f) | (codepoint << 6) :
+      (0xff >>> type) & byte;
+    state = h$_text_utf8d[256 + state + type];
+    return state;
+  }
+  while (srco < src_end_o) {
+    if(decode(src_v.getUint8(srco++)) !== h$_text_UTF8_ACCEPT) {
+      if(state !== h$_text_UTF8_REJECT) {
+        continue;
+      } else {
+        break;
+      }
+    }
+    if (codepoint <= 0xffff) {
+      dest_v.setUint16(dsto,codepoint);
+      dsto += 2;
+    } else {
+      dest_v.setUint16(dsto,(0xD7C0 + (codepoint >> 10)));
+      dest_v.setUint16(dsto+2,(0xDC00 + (codepoint & 0x3FF)));
+      dsto += 4;
+    }
+  }
+
+  /* Error recovery - if we're not in a valid finishing state, back up. */
+  if (state != h$_text_UTF8_ACCEPT)
+    srco -= 1;
+
+  destoff_v.setUint32(destoff_o,dsto>>1);
+  h$ret1 = srco;
+  return src_v;
+}
+
