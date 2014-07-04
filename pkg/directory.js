b55a77df8ea6626c0b6ea7698bfb289acf300e0a
Luite Stegeman <stegeman@gmail.com>
2014-07-04 12:08:51 +0200

    preliminary process and directory support

---
+++ pkg/directory.js
@@ -0,0 +1,249 @@
+//#define GHCJS_NODE 1
+//#ifdef GHCJS_NODE
+// only works on node.js
+
+#include "HsBaseConfig.h"
+
+#ifdef GHCJS_TRACE_DIRECTORY
+function h$logDirectory() { h$log.apply(h$log,arguments); }
+#define TRACE_DIRECTORY(args...) h$logDirectory(args)
+#else
+#define TRACE_DIRECTORY(args...)
+#endif
+
+if(typeof module !== 'undefined' && module.exports) {
+    var h$fs      = require('fs');
+    var h$path    = require('path');
+    var h$os      = require('os');
+    var h$process = process;
+}
+
+function h$directory_setErrno(e) {
+  TRACE_PROC("setErrno: " + e);
+  var es = e.toString();
+  var getErr = function() {
+      if(es.indexOf('ENOTDIR') !== -1)      return CONST_ENOTDIR;
+      if(es.indexOf('ENOENT') !== -1)       return CONST_ENOENT;
+      if(es.indexOf('EEXIST') !== -1)       return CONST_EEXIST;
+      if(es.indexOf('ENETUNREACH') !== -1)  return CONST_EINVAL; // fixme
+      if(es.indexOf('EPERM') !== -1)        return CONST_EPERM;
+      if(es.indexOf('Bad argument') !== -1) return CONST_ENOENT; // fixme?
+      throw ("setErrno not yet implemented: " + e);
+
+  }
+  h$errno = getErr();
+}
+
+function h$directory_handleErrno(f) {
+  try {
+    f();
+  } catch(e) {
+    h$directory_setErrno(e);
+    return -1;
+  }
+  return 0;
+}
+
+function h$directory_handleErrnoM1(f) {
+  try {
+    return f();
+  } catch(e) {
+    h$directory_setErrno(e);
+  }
+  return -1;
+}
+
+
+function h$directory_handleErrnoNull(f) {
+  try {
+    return f();
+  } catch(e) {
+    h$directory_setErrno(e);
+  }
+  return null;
+}
+
+// get/set permissions for file
+// set errno and return -1 on error
+// masks: 1 - read
+//        2 - write
+//        4 - exe
+//        8 - search
+function h$directory_getPermissions(file) {
+  TRACE_DIRECTORY("getPermissions: " + file);
+  return h$directory_handleErrnoM1(function() {
+    var fs = h$fs.statSync(file);
+    var m = fs.mode;
+    var r = (m&4) || (m&32) || (m&256);
+    var w = (m&2) || (m&16) || (m&128);
+    var x = (m&1) || (m&8)  || (m&64);
+    var exe    = x; // fixme?
+    var search = x; // fixme?
+    return ((r?1:0)|(w?2:0)|(exe?4:0)|(search?8:0));
+  });
+}
+
+function h$directory_setPermissions(file, perms) {
+  TRACE_DIRECTORY("setPermissions: " + file + " " + perms);
+  return h$directory_handleErrno(function() {
+    var fs = h$fs.statSync(file);
+    var r = perms & 1;
+    var w = perms & 2;
+    var x = perms & 4;
+    var search = perms & 8;
+    var m  = fs.mode;
+    m = r ? (m | 292) : (m & ~292);
+    m = w ? (m | 146) : (m & ~146);
+    m = (x || search) ? (m | 73) : (m & ~73);
+    h$fs.chmodSync(file, m);
+  });
+}
+
+function h$directory_copyPermissions(file1, file2) {
+  TRACE_DIRECTORY("copyPermissions: " + file1 + " " + file2);
+  return h$directory_handleErrno(function() {
+    var fs = h$fs.statSync(file1);
+    h$fs.chmodSync(file2, fs.mode);
+  });
+}
+
+
+function h$directory_createDirectory(dir) {
+  TRACE_DIRECTORY("createDirectory: " + dir);
+  return h$directory_handleErrno(function() {
+    h$fs.mkdirSync(dir);
+  });
+}
+
+function h$directory_removeDirectory(dir) {
+  TRACE_DIRECTORY("removeDirectory: " + dir);
+  return h$directory_handleErrno(function() {
+    h$fs.rmdirSync(dir);
+  });
+}
+
+function h$directory_removeFile(file) {
+  TRACE_DIRECTORY("removeFile: " + file);
+  return h$directory_handleErrno(function() {
+    h$fs.unlinkSync(file);
+  });
+}
+
+function h$directory_renameDirectory(dir1, dir2) {
+  TRACE_DIRECTORY("renameDirectory: " + dir1 + " " + dir2);
+  return h$directory_handleErrno(function() {
+    h$fs.renameSync(dir1, dir2);
+  });
+}
+
+function h$directory_renameFile(file1, file2) {
+  TRACE_DIRECTORY("renameFile: " + file1 + " " + file2);
+  return h$directory_handleErrno(function() {
+    h$fs.renameSync(file1, file2);
+  });
+}
+
+function h$directory_canonicalizePath(path) {
+  TRACE_DIRECTORY("canonicalizePath: " + path);
+  return h$path.normalize(path);
+}
+
+function h$directory_findExecutables(name) {
+  TRACE_DIRECTORY("findExecutables: " + name);
+  var result = [];
+  var parts = h$process.env.PATH.split((h$os.platform() === 'windows')?';':':');
+  for(var i=0;i<parts.length;i++) {
+    try {
+      var file = parts[i] + h$path.sep + name;
+      TRACE_DIRECTORY("trying: " + file);
+      var fs   = h$fs.statSync(file);
+      if((fs.mode & 73) || h$os.platform() === 'windows') result.push(file);
+    } catch(e) { }
+  }
+  return result;
+}
+
+function h$directory_getDirectoryContents(dir) {
+  TRACE_DIRECTORY("getDirectoryContents: " + dir);
+  return h$directory_handleErrnoNull(function() {
+    return h$fs.readdirSync(dir);
+  });
+}
+
+function h$directory_getCurrentDirectory() {
+  TRACE_DIRECTORY("getCurrentDirectory");
+  return h$directory_handleErrnoNull(function() {
+    return h$process.cwd();
+  });
+}
+
+function h$directory_setCurrentDirectory(dir) {
+  TRACE_DIRECTORY("setCurrentDirectory: " + dir);
+  return h$directory_handleErrno(function() {
+    return h$process.chdir(dir);
+  });
+}
+
+function h$directory_getHomeDirectory(dir) {
+  TRACE_DIRECTORY("getHomeDirectory: " + dir);
+  return h$process.env.HOME ||
+         h$process.env.HOMEPATH ||
+         h$process.env.USERPROFILE;
+}
+
+function h$directory_getAppUserDataDirectory(appName) {
+    TRACE_DIRECTORY("getAppUserDataDirectory: " + appName);
+    if(process.env.APPDATA)
+        return h$process.env.APPDATA + h$path.sep + appName;
+    if(process.env.HOME)
+        return h$process.env.HOME + h$path.sep + "." + appName;
+    TRACE_DIRECTORY("getAppUserDataDirectory fallback");
+    return "/";
+}
+
+function h$directory_getTemporaryDirectory() {
+  TRACE_DIRECTORY("getTemporaryDirectory");
+  return h$directory_handleErrnoNull(function() {
+    return h$os.tmpdir();
+  });
+}
+
+function h$directory_exeExtension() {
+  TRACE_DIRECTORY("exeExtension");
+  return (h$os.platform() === 'windows') ? 'exe' : '';
+}
+
+function h$directory_getFileStatus(file) {
+  TRACE_DIRECTORY("getFileStatus: " + file);
+  return h$directory_handleErrnoNull(function() {
+    return h$fs.statSync(file);
+  });
+}
+
+function h$directory_getFileOrSymlinkStatus(file) {
+    TRACE_DIRECTORY("getFileOrSymlinkStatus: " + file);
+    return h$directory_handleErrnoNull(function() {
+        return h$fs.lstatSync(file);
+    });
+}
+
+function h$directory_getFileStatusModificationTime(fs) {
+    TRACE_DIRECTORY("getFileStatusModificationTime: " + fs.mtime.getTime());
+    return fs.mtime.getTime();
+}
+
+function h$directory_getFileStatusIsDirectory(fs) {
+  TRACE_DIRECTORY("getFileStatusIsDirectory: " + fs + " " + fs.isDirectory());
+  return fs.isDirectory();
+}
+
+// fixme this doesn't really belong here
+function h$chmod(path_d, path_o, m) {
+    var path = h$decodeUtf8z(path_d, path_o);
+    TRACE_DIRECTORY("chmod: " + path + " mode: " + m);
+    h$fs.chmodSync(path, m);
+    return 0;
+}
+
+//#endif
+
---
+++ pkg/process.js
@@ -0,0 +1,261 @@
+// #ifdef GHCJS_NODE
+// only works on node.js
+
+#ifdef GHCJS_TRACE_PROCESS
+function h$logProcess() { h$log.apply(h$log,arguments); }
+#define TRACE_PROCESS(args...) h$logProcess(args)
+#else
+#define TRACE_PROCESS(args...)
+#endif
+
+if(typeof module !== 'undefined' && module.exports) {
+    var h$child = require('child_process');
+}
+
+// one-dir pipe
+function h$pipeFd(pipe, write) {
+  var buf = { pipe: pipe
+            };
+  var fd = new h$Fd(buf, false, write, false);
+  h$fds[fd.fd] = fd;
+  TRACE_PROCESS("pipe " + fd.fd + " opened, writable: " + write);
+  if(write) {
+    buf.read = function() {
+      throw "cannot read from write pipe";
+    };
+    buf.close = function() { /* pipe.close(); */ };
+    buf.write = function(fd, buf, buf_offset, n)  {
+      TRACE_PROCESS("pipe " + fd.fd + " write: " + n);
+      if(!fd.writeReady) throw "pipe is not writable, please wait!"
+      fd.writeReady = false;
+      var u8 = buf.u8;
+      var nbuf = new Buffer(n);
+      // can this be made more efficient?
+      for(var k=0;k<n;k++) {
+        nbuf[k] = buf[buf_offset+k];
+      }
+      pipe.write(nbuf, function() {
+        fd.writeReady = true;
+        h$notifyWrite(fd);
+      });
+      return n;
+    };
+  } else {
+    buf.blocksReady = false;
+    buf.chunks = new h$Queue();
+    buf.chunkOff = 0;
+    buf.eof = false;
+    buf.eofPending = false;
+    buf.close = function() { /* pipe.close(); */ };
+    pipe.on('readable', function() {
+      TRACE_PROCESS("pipe " + fd.fd + " readable");
+      var ch = pipe.read();
+      if(ch) buf.chunks.enqueue(ch);
+      fd.readReady = true;
+//      buf.blocksReady = true;
+      h$notifyRead(fd);
+    });
+    pipe.on('end', function() {
+      TRACE_PROCESS("pipe " + fd.fd + " eof");
+      buf.eofPending = true;
+      fd.readReady = true;
+      h$notifyRead(fd);
+    });
+    buf.write = function() {
+      throw "cannot write to read pipe";
+    };
+    buf.read = function(fd, buf, buf_offset, n) {
+      TRACE_PROCESS("pipe " + fd.fd + " read: " + n + " (" + fd.fd + ") " + fd.readReady);
+      if(fd.buf.chunks.getCount() === 0) {
+        TRACE_PROCESS("pipe " + fd.fd + " adding chunk to buffer " + fd.fd);
+        var ch = pipe.read();
+        if(ch) {
+          do {
+            fd.buf.chunks.enqueue(ch);
+          } while((ch = pipe.read()) !== null);
+          fd.buf.blocksReady = false;
+        } else {
+          TRACE_PROCESS("pipe " + fd.fd + " WARNING returning zero bytes, eof status: " + fd.buf.eof);
+          if(fd.buf.eofPending) fd.buf.eof = true;
+            fd.buf.readReady = false;
+            if(fd.buf.eof) {
+                return 0;
+            } else {
+                h$errno = CONST_EAGAIN;
+                return -1;
+            }
+        }
+      }
+      var h = fd.buf.chunks.peek();
+      var o = fd.buf.chunkOff;
+      var left = h.length - o;
+      var u8 = buf.u8;
+      TRACE_PROCESS("pipe " + fd.fd + " reading " + h.length + " " + o);
+      if(left > n) {
+        for(var i=0;i<n;i++) {
+          u8[buf_offset+i] = h[o+i];
+        }
+        fd.buf.chunkOff += n;
+        TRACE_PROCESS("pipe " + fd.fd + " read " + n + " bytes");
+        return n;
+      } else {
+        for(var i=0;i<left;i++) {
+          u8[buf_offset+i] = h[o+i];
+        }
+        fd.buf.chunkOff = 0;
+        fd.buf.chunks.dequeue();
+        TRACE_PROCESS("pipe " + fd.fd + " chunk count: " + (fd.buf.chunks.getCount() === 0) + " eof: " + fd.buf.eof + " blocksReady: " + fd.buf.blocksReady);
+        if(fd.buf.chunks.getCount() === 0 && !fd.buf.blocksReady) {
+          if(!fd.buf.eof && !fd.buf.eofPending) {
+            TRACE_PROCESS("pipe " + fd.fd + " read, end of input reached");
+            fd.readReady = false;
+          } else {
+            TRACE_PROCESS("pipe " + fd.fd + " read, eof reached");
+            if(fd.buf.eofPending) fd.buf.eof = true;
+          }
+        }
+        TRACE_PROCESS("pipe " + fd.fd + " read " + left + " bytes (remainder of chunk)");
+        return left;
+      }
+    };
+  }
+  TRACE_PROCESS("created pipe, fd: " + fd.fd);
+  return fd;
+}
+
+function h$process_runInteractiveProcess( cmd, args, workingDir, env
+                                        , stdin_fd, stdout_fd, stderr_fd
+                                        , closeHandles, createGroup, delegateCtlC) {
+  TRACE_PROCESS("runInteractiveProcess");
+  TRACE_PROCESS("cmd: " + cmd + " args: " + args);
+  TRACE_PROCESS("workingDir: " + workingDir + " env: " + env);
+  TRACE_PROCESS("stdin: " + stdin_fd + " stdout: " + stdout_fd + " stderr: " + stderr_fd);
+
+  var stdin_p, stdout_p, stderr_p;
+
+  if(stdin_fd === -1) {
+    stdin_p = 'pipe';
+  } else if(stdin_fd === 0) {
+    stdin_p = process.stdin;
+  } else {
+    throw "runInteractiveProcess: custom stdin unsupported";
+  }
+
+  if(stdout_fd === -1) {
+    stdout_p = 'pipe';
+  } else if(stdout_fd === 1) {
+    stdout_p = process.stdout;
+  } else {
+    throw "runInteractiveProcess: custom stdout unsupported";
+  }
+
+  if(stderr_fd === -1) {
+    stderr_p = 'pipe'
+  } else if(stderr_fd === 2) {
+    stderr_p = process.stderr;
+  } else {
+    throw "runInteractiveProcess: custom stderr unsupported";
+  }
+
+  var options = { detached: createGroup
+                , stdio: [stdin_p, stdout_p, stderr_p]
+                };
+  if(workingDir !== null) options.cwd = workingDir;
+  if(env !== null) {
+    var envObj = {};
+    for(var i=0;i<env.length;i+=2) envObj[env[i]] = env[i+1];
+    TRACE_PROC("environment: " + h$collectProps(envObj));
+    options.env = envObj;
+  }
+
+  var procObj;
+  var child = h$child.spawn(cmd, args, options);
+  child.on('exit', function(code, sig) {
+    TRACE_PROC("process finished: " + code + " " + sig);
+    procObj.exit = code;
+    for(var i=0;i<procObj.waiters.length;i++) {
+      procObj.waiters[i](code);
+    }
+  });
+
+  // fixme this leaks
+  procObj = { pid: h$nProc
+            , fds: [ stdin_fd  === -1 ? h$pipeFd(child.stdin, true).fd   : 0
+                   , stdout_fd === -1 ? h$pipeFd(child.stdout, false).fd : 1
+                   , stderr_fd === -1 ? h$pipeFd(child.stderr, false).fd : 2
+                   ]
+            , exit: null
+            , waiters : []
+            , child: child
+            };
+
+  h$procs[h$nProc++] = procObj;
+
+  return procObj;
+}
+
+var h$nProc = 1;
+var h$procs = [];
+
+
+// return the thing to run as an array, first element the process, rest the args
+// null if no interpreter can be found
+function h$process_commandToProcess(cmd, args) {
+  TRACE_PROCESS("commandToProcess: " + cmd + " " + args);
+  if(h$os.platform() !== 'windows') {
+    if(args === null) { // shellcmd
+      var com = h$process.env.COMSPEC;
+      if(!com) {
+        com = h$directory_findExecutables("cmd.exe");
+        if(com.length) {
+          com = cmd[0];
+        } else {
+          com = h$directory_findExecutables("command.com");
+          if(!com.length) return null;
+          com = com[0];
+        }
+      }
+      // fixme need to escape stuff
+      return [com, com + " /c " + args];
+    } else {
+       // fixme need to escape stuff
+       return [cmd, cmd, args.join(' ')];
+    }
+  } else {  // non-windows
+    if(args === null) { // shellcmd
+      return ["/bin/sh", "-c", cmd];
+    } else {
+       var r = [cmd];
+       r.push(args);
+       return r;
+    }
+  }
+}
+
+function h$process_terminateProcess(pid) {
+  TRACE_PROCESS("terminateProcess: " + pid);
+  var p = h$procs[pid];
+  p.child.kill();
+  return 0; // fixme error status?
+}
+
+function h$process_getProcessExitCode(pid, code_d, code_o) {
+  TRACE_PROCESS("getProcessExitCode: " + pid);
+  var p = h$procs[pid];
+  if(p.exit === null) return 0;
+  code_d.i3[code_o] = p.exit;
+  return 1;
+}
+
+function h$process_waitForProcess(pid, code_d, code_o, c) {
+  TRACE_PROCESS("waitForProcess: " + pid);
+  var p = h$procs[pid];
+  if(p.exit !== null) {
+    h$process_getProcessExitCode(pid, code_d, code_o);
+    c(0);
+  } else {
+    p.waiters.push(c);
+  }
+}
+
+// #endif // GHCJS_NODE
