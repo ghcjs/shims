b065925fc921d1e61a860691f80769a8a34ad25c
Luite Stegeman <stegeman@gmail.com>
2014-07-17 03:18:09 +0200

    add complete unicode tables, change packing method

--- src/string.js
+++ src/string.js
@@ -44,34 +44,31 @@ function h$urstra(arr) { return h$toHsList(arr); }
 function h$urstr(arr) { return h$toHsStringMU8(arr); }
 #endif
 
+function h$caseMapping(x) {
+    return (x%2)?-((x+1)>>1):(x>>1);
+}
 
 var h$toUpper = null;
 function h$u_towupper(ch) {
-  if(h$toUpper == null) { h$toUpper = h$decodeMapping(h$toUpperMapping); }
-  var r = h$toUpper[ch];
-  return (r !== null && r !== undefined) ? r : ch;
+    if(h$toUpper == null) { h$toUpper = h$decodeMapping(h$toUpperMapping, h$caseMapping); }
+    return ch+(h$toUpper[ch]|0);
 }
 
 var h$toLower = null;
 function h$u_towlower(ch) {
-  if(h$toLower == null) { h$toLower = h$decodeMapping(h$toLowerMapping); }
-  var r = h$toLower[ch];
-  return (r !== null && r !== undefined) ? r : ch;
-}
-
-function h$u_iswspace(ch) {
-  return /^\s$/.test(new String(ch)) ? 1 : 0;
+    if(h$toLower == null) { h$toLower = h$decodeMapping(h$toLowerMapping, h$caseMapping); }
+    return ch+(h$toLower[ch]|0);
 }
 
 var h$alpha = null;
 function h$u_iswalpha(a) {
-  if(h$alpha == null) { h$alpha = h$decodeRle(h$alphaRanges); }
-  return h$alpha[a] == 1 ? 1 : 0;
+    if(h$alpha == null) { h$alpha = h$decodeRLE(h$alphaRanges); }
+    return h$alpha[a]|0;
 }
 
 var h$alnum = null;
 function h$u_iswalnum(a) {
-  if(h$alnum == null) { h$alnum = h$decodeRle(h$alnumRanges); }
+  if(h$alnum == null) { h$alnum = h$decodeRLE(h$alnumRanges); }
   return h$alnum[a] == 1 ? 1 : 0;
 }
 
@@ -82,14 +79,14 @@ function h$u_iswspace(a) {
 
 var h$lower = null;
 function h$u_iswlower(a) {
-  if(h$lower == null) { h$lower = h$decodeRle(h$lowerRanges); }
-  return h$lower[a] == 1 ? 1 : 0;
+    if(h$lower == null) { h$lower = h$decodeRLE(h$lowerRanges); }
+    return h$lower[a]|0;
 }
 
 var h$upper = null;
 function h$u_iswupper(a) {
-  if(h$upper == null) { h$upper = h$decodeRle(h$upperRanges); }
-  return h$upper[a] == 1 ? 1 : 0;
+    if(h$upper == null) { h$upper = h$decodeRLE(h$upperRanges); }
+    return h$upper[a]|0;
 }
 
 
@@ -98,61 +95,100 @@ function h$u_iswcntrl(a) {
   return (h$cntrl.indexOf(a) !== -1) ? 1 : 0;
 }
 
-// rle: start,length pairs
 var h$print = null;
 function h$u_iswprint(a) {
-  if(h$print == null) { h$print = h$decodeRle(h$printRanges); }
-  return h$print[a] === 1 ? 1 : 0;
+    if(h$print == null) { h$print = h$decodeRLE(h$printRanges); }
+    return h$print[a]|0;
 }
 
-// decode rle array of start/length pairs
-function h$decodeRle(arr) {
-  var r = [];
-  for(var i=0;i<arr.length;i+=2) {
-    var start = arr[i];
-    var length = arr[i+1];
-    for(var j=start;j<start+length;j++) {
-      r[j] = 1;
+// decode a packed string (Compactor encoding method) to an array of numbers
+function h$decodePacked(s) {
+    function f(o) {
+        var c = s.charCodeAt(o);
+        return c<34?c-32:c<92?c-33:c-34;
     }
-  }
-  return r;
+    var r=[], i=0;
+    while(i < s.length) {
+        var c = s.charCodeAt(i);
+        if(c < 124) r.push(f(i++));
+        else if(c === 124) {
+            i += 3; r.push(90+90*f(i-2)+f(i-1));
+        } else if(c === 125) {
+            i += 4;
+            r.push(8190+8100*f(i-3)+90*f(i-2)+f(i-1));
+        } else throw ("h$decodePacked: invalid: " + c);
+    }
+    return r;
 }
 
-// decode a mapping array of from/to pairs
-function h$decodeMapping(arr) {
-  var r = [];
-  for(var i=0;i<arr.length;i+=2) {
-    var from = arr[i];
-    var to   = arr[i+1];
-    r[from] = to;
-  }
-  return r;
+// decode string with encoded character ranges
+function h$decodeRLE(str) {
+    var r = [], x = 0, i = 0, j = 0, v, k, a = h$decodePacked(str);
+    while(i < a.length) {
+        v = a[i++];
+        if(v === 0) { // alternating
+            k = a[i++];
+            while(k--) {
+                r[j++] = x;
+                r[j++] = 1-x;
+            }
+        } else {
+            if(v <= 2) {
+                k = (a[i]<<16)+a[i+1];
+                i+=2;
+            } else k = (v-1)>>1;
+            if(v%2) {
+                r[j++] = x;
+                x = 1-x;
+            }
+            if(x) while(k--) r[j++] = x; else j+=k;
+            x = 1-x;
+        }
+    }
+    r.shift();
+    return r;
+}
+
+function h$decodeMapping(str, f) {
+    var r = [], i = 0, j = 0, k, v, v2, a = h$decodePacked(str);
+    while(i < a.length) {
+        v = a[i++];
+        if(v === 0) { // alternating
+            k  = a[i];
+            v  = f(a[i+1]);
+            v2 = f(a[i+2]);
+            while(k--) {
+                r[j++] = v;
+                r[j++] = v2;
+            }
+            i+=3;
+        } else {
+            if(v === 2) {
+                k = (a[i] << 16) + a[i+1];
+                v = a[i+2];
+                i += 3;
+            } else if(v%2) {
+                k = 1;
+                v = v>>1;
+            } else {
+                k = (v>>1)-1;
+                v = a[i++];
+            }
+            v = f(v);
+            if(v) while(k--) r[j++] = v; else j+=k;
+        }
+    }
+    return r;
 }
 
 var h$unicodeCat = null;
 function h$u_gencat(a) {
-    if(h$unicodeCat == null) {
-        h$unicodeCat = [];
-        // concatMap (\run ->
-        //    case run of
-        //        [a]   -> [chr (a+64)]
-        //        (a:_) -> show (length run) ++ [chr (a+64)]) . group $ map (fromEnum . generalCategory) ['\x00'..'\xFFFF']
-        var s = "32YV3QS3QMNQRQL2Q10H2Q3R2Q26@MQNTKT26AMRNR33YVQ4S2UTUAORZUTUR2JTAUQTJAP3JQ23@R7@24AR8A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@2A@A@A@A@A@A@A@A@2A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A2@A@A@3A2@A@A2@A3@2A4@A2@A3@3A2@A2@A@A@A2@A@2A@A2@A3@A@A2@2AD@3A4D@BA@BA@BA@A@A@A@A@A@A@A@2A@A@A@A@A@A@A@A@A@2A@BA@A3@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@7A2@A2@2A@A4@A@A@A@A@69AD27A18C4T12C14T5C7TCTC17T112E@A@ACT@A2]C3AQ5]2T@Q3@]@]2@A17@]9@35A@2A3@3A@A@A@A@A@A@A@A@A@A@A@A@5A@AR@A2@2A51@48A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@AU5E2G@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A2@A@A@A@A@A@A@2A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A9]38@2]C6Q]39A]QL6]45ELEQ2EQ2EQE8]27D5]3D2Q11]4Z2]3R2QS2Q2U11EQ2]2Q32DC10D21E10H4Q2DE99DQD7EZU6E2C2EU4E2D10H3D2UD14Q]ZDE30D27E2]89D11ED14]10H33D9E2CU3QC5]22D4EC9EC3EC5E2]15Q]25D3E2]Q161]3EF54DEFED3F8E4FE2FD7E10D2E2Q10HQC6D]7D]E2F]8D2]2D2]22D]7D]D3]4D2]ED3F4E2]2F2]2FED8]F4]2D]3D2E2]10H2D2S6JUS5]2EF]6D4]2D2]22D]7D]2D]2D]2D2]E]3F2E4]2E2]3E3]E7]4D]D7]10H2E3DE11]2EF]9D]3D]22D]7D]2D]5D2]ED3F5E]2EF]2FE2]D15]2D2E2]10H]S15]E2F]8D2]2D2]22D]7D]2D]5D2]EDFEF4E2]2F2]2FE8]EF4]2D]3D2E2]10HUD6J10]ED]6D3]3D]4D3]2D]D]2D3]2D3]3D3]12D4]2FE2F3]3F]3FE2]D6]F14]10H3J6USU6]3F]8D]3D]23D]10D]5D3]D3E4F]3E]4E7]2E]2D6]2D2E2]10H8]7JU2]2F]8D]3D]23D]10D]5D2]EDFE5F]E2F]2F2E7]2F7]D]2D2E2]10H]2D15]2F]8D]3D]41D2]D3F4E]3F]3FED8]F8]2D2E2]10H6J3]U6D2]2F]18D3]24D]9D]D2]7D3]E4]3F3E]E]8F18]2FQ12]48DE2D7E4]S6DC8EQ10H2Q37]2D]D2]2D]D2]D6]4D]7D]3D]D]D2]2D]4DE2D6E]2ED2]5D]C]6E2]10H2]2D34]D3U15Q5U2E6U10H10JUEUEUEMNMN2F8D]36D4]14EF5EQ2E5D11E]36E]8UE6U]2U5Q4U2Q37]43D2F4EF6EF2E2F2ED10H6Q6D2F2E4D3ED3F2D7F3D4E13DE2F2E6FEDF10H3FE2U38@10]43DQC3]329D]4D2]7D]D]4D2]41D]4D2]33D]4D2]7D]D]4D2]15D]57D]4D2]67D2]3EU8Q20J3]16D10U6]85D11]L620D2Q17DV26DMN3]75D3Q3I15]13D]4D3E11]18D3E2Q9]18D2E12]13D]3D]2E12]52D2ZF7E8FE2F11E3QC3QSDE2]10H6]10J6]6QL4Q3EV]10H6]35DC52D8]41DED5]70D10]29D3]3E4F2E3F4]2FE6F3E4]U3]2Q10H30D2]5D11]44D4]17F7D2F6]10HJ3]34U23D2E3F2]2Q53DFEF7E]EFE2F8E6F10E2]E10H6]10H6]7QC6Q82]4EF47DEF5EFE5FE2F7D4]10H7Q10U9E9U3]2EF30DF4E2F2EF3]2D10H6]38DEF2E3FEF3E2F8]4Q36D8F8E2F2E3]5Q10H3]3D10H30D6C2Q80]3EQ13EF7E4DE4DF13]44A54C22AC34A37C39E21]4E@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@9A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@9A8@6A2]6@2]8A8@8A8@6A2]6@2]8A]@]@]@]@8A8@14A2]8A8B8A8B8A8B5A]2A4@BTA3T3A]2A4@B3T4A2]2A4@]3T8A5@3T2]3A]2A4@B2T]11V5Z6L2QOPM2OPMO8QWX5ZV9QOP4Q2K3QRMN11QRQK10QV5Z5]6ZJC2]6J3RMNC10J3RMN]13C3]26S22]13E4GE3G12E15]2U@4U@2UA3@2A3@AU@2UR5@6U@U@U@U4@UA4@A4DA2U2A2@5R@4AUR2UAU16J35I@A4IJ6]5R5U2R4UR2UR2UR7UR31U2R2URUR31U268R8U4R20U2R7UMN81UR30U25R40U6R18U12]39U25]11U21]60J78U22J183UR9UR54U8R111UR144U]103UMNMNMNMNMNMNMN30J44U5RMN4R]R]24RMNMNMNMNMN16R256U131RMNMNMNMNMNMNMNMNMNMNMN63RMNMN32RMN258R48U21R2U6R3]10U166]47@]47A]@A3@2A@A@A@A4@A@2A@7AC3@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@2A6U@A@A3E7]4QJ2Q38A10]54D9]CQ14]E23D9]7D]7D]7D]7D]7D]7D]7D]7D]32E2QOPOP3QOPQOP9QL2QLQOP2QOPMNMNMNMN5QC2Q78]26U]89U12]214U26]12U4]V3QUCDIMNMNMNMNMN2UMNMNMNMNLM2NU9I6EL5C2U3ICDQ2U]86D2]2E2T2CDL90DQ3CD5]41D3]94D]2U4J10U27D5]36U12]16D31U]10J39U15J32U10J39U15J63U]256U6582D10]64U20940D52]21DC1143D3]55U9]40D6C2Q268DC3Q16D10H2D20]@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@ADE3GQ8]2EQC@A@A@A@A@A@A@A@A@A@A@A@A8]70D10I2E6Q8]23T9C2T@A@A@A@A@A@A@3A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@A@AC8A@A@A2@A@A@A@A@AC2T@A@A]@A14]@A@A@A@A@A80]A7DE3DE4DE23D2F2EF4U4]6J2USU6]52D4Q8]2F50D16FE9]2Q10H6]18E6D3QD4]10H28D8E2Q23D11E2F11]Q29D3]3EF47DE2F4E2FE4F13Q]C10H4]2Q32]41D6E2F2E2F2E9]3DE8DEF2]10H2]4Q16DC6D3UDF4]48DED3E2D2E5D2EDED24]2DC2Q33]6D2]6D2]6D9]7D]7D145]35D2FE2FE2FQFE2]10H6]11172D12]23D4]49D4]2048[6400\\302D2]62D2]106D38]7A12]5A5]DE10DR13D]5D]D]2D]2D]108D16T17]363DMN16]64D2]54D40]12DSU2]16E7QMNQ6]7E9]Q2L2KMNMNMNMNMNMNMNMN2QMN4Q3K3Q]4QLMNMNMN3QRL3R]QS2Q4]5D]135D2]Z]3QS3QMNQRQL2Q10H2Q3R2Q26@MQNTKT26AMRNRMNQMN2Q10DC45D2C31D3]6D2]6D2]6D2]3D3]2SRTU2S]U4R2U10]3Z2U2]";
-        for(var n = 0; n != s.length; n++) {
-            var l = '';
-            while(strIsNumericAt(s,n)) {
-                l = l + s[n++];
-            }
-            l = (l === '') ? 1 : (l | 0);
-            var c = s[n].charCodeAt() - 64;
-            for(var x = 0; x !== l; x++)
-                h$unicodeCat[h$unicodeCat.length] = c;
-        }
-    }
-    function strIsNumericAt(s,n) { var ch = s.charCodeAt(n); return ch >= 48 && ch <= 57; }
-    return h$unicodeCat[a]|0;
+    if(h$unicodeCat == null) h$unicodeCat = h$decodeMapping(h$catMapping, function(x) { return x; });
+    // private use
+    if(a >= 0xE000 && a <= 0xF8FF || a >= 0xF0000 & a <= 0xFFFFD || a >= 0x100000 && a <= 0x10FFFD) return 28;
+    var c = h$unicodeCat[a]|0;
+    return c?c-1:29;
 }
-
 
 function h$localeEncoding() {
     //   h$log("### localeEncoding");
@@ -497,6 +533,7 @@ function h$toHsString(str, cc) {
 #else
 function h$toHsString(str) {
 #endif
+  if(typeof str !== 'string') return h$ghczmprimZCGHCziTypesziZMZN;
   var i = str.length - 1;
   var r = h$ghczmprimZCGHCziTypesziZMZN;
   while(i>=0) {
@@ -545,6 +582,7 @@ function h$toHsStringA(str, cc) {
 #else
 function h$toHsStringA(str) {
 #endif
+  if(typeof str !== 'string') return h$ghczmprimZCGHCziTypesziZMZN;
   var i = str.length - 1;
   var r = h$ghczmprimZCGHCziTypesziZMZN;
   while(i>=0) {
