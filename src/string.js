841c7909f67204d8591bf8e5ebab44d616ba8fa8
Luite Stegeman <stegeman@gmail.com>
2014-12-25 03:40:54 +0100

    update unicode tables

--- src/string.js
+++ src/string.js
@@ -73,7 +73,7 @@ function h$u_iswalnum(a) {
 }
 
 function h$u_iswspace(a) {
-    return '\t\n\v\f\r \u0020\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000'
+    return '\t\n\v\f\r \u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000'
         .indexOf(String.fromCharCode(a)) !== -1 ? 1 : 0;
 }
 
