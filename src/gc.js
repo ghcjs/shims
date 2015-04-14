fbddb259767b72e673d0803938b9390a332befba
Luite Stegeman <stegeman@gmail.com>
2015-04-14 16:40:15 +1200

    fix marking of non-deadlocked threads waiting on an MVar

--- src/gc.js
+++ src/gc.js
@@ -209,17 +209,7 @@ function h$gc(t) {
     // throw an exception to a thread (which brings it back
     // to life), then scan it. Killing one thread might be enough
     // since the killed thread could make other threads reachable again.
-    var killedThread;
-    while(killedThread = h$finalizeMVars()) {
-        h$markThread(killedThread);
-        h$markRetained();
-    }
-
-    // mark all blocked threads
-    iter = h$blocked.iter();
-    while((nt = iter.next()) !== null) h$markThread(nt);
-
-    // and their weak references etc
+    h$finalizeMVars();
     h$markRetained();
 
     // now everything has been marked, bring out your dead references
@@ -485,21 +475,29 @@ function h$resetThread(t) {
     TRACE_GC("h$resetThread: " + (Date.now()-start) + "ms");
 }
 
-// throw blocked indefinitely exception to the first thread waiting on an unreferenced MVar
+
 function h$finalizeMVars() {
     TRACE_GC("finalizing MVars");
-    var i, t, iter = h$blocked.iter();
-    while((t = iter.next()) !== null) {
-        if(t.status === h$threadBlocked && t.blockedOn instanceof h$MVar) {
-            // if h$unboxFFIResult is the top of the stack, then we cannot kill
-            // the thread since it's waiting for async FFI
-            if(t.blockedOn.m !== h$gcMark && t.stack[t.sp] !== h$unboxFFIResult) {
-                h$killThread(t, h$ghcjszmprimZCGHCJSziPrimziInternalziblockedIndefinitelyOnMVar);
-                return t;
-            }
+    var kill, i, t, iter;
+    do {
+	kill = null;
+	iter = h$blocked.iter();
+	while((t = iter.next()) !== null) {
+            if(t.status === h$threadBlocked && t.blockedOn instanceof h$MVar) {
+		// if h$unboxFFIResult is the top of the stack, then we cannot kill
+		// the thread since it's waiting for async FFI
+		if(t.blockedOn.m !== h$gcMark && t.stack[t.sp] !== h$unboxFFIResult) {
+		    kill = t;
+		} else  {
+		    h$markThread(t);
+		}
+	    }
         }
-    }
-    return null;
+	if(kill && kill.blockedOn.m !== h$gcMark) {
+	    h$killThread(kill, h$ghcjszmprimZCGHCJSziPrimziInternalziblockedIndefinitelyOnMVar);
+	    h$markThread(kill);
+	}
+    } while(kill);
 }
 
 // clear DOM retainers
