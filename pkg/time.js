fa64cf854921588fb234d5a2270f1922da415571
Luite Stegeman <stegeman@gmail.com>
2016-08-05 16:42:19 +0200

    fix Date.now() conversion in clock_gettime

--- pkg/time.js
+++ pkg/time.js
@@ -21,8 +21,8 @@ function h$clock_gettime(when, p_d, p_o) {
 
   var o  = p_o >> 2,
       t  = Date.now ? Date.now() : new Date().getTime(),
-      tf = Math.floor(t),
-      tn = 1000000000 * (t - tf);
+      tf = Math.floor(t / 1000),
+      tn = 1000000 * (t - (1000 * tf));
   p_d.i3[o]   = tf|0;
   p_d.i3[o+1] = tn|0;
   return 0;
